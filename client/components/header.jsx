d982c56c0bfc9cf458c80cffb49f3416403a46b8
Edward Kye <edwardkye@me.com>
2018-07-17 10:33:40 -0400

    add header/navbar with buttons and log in

---
+++ client/components/header.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Navbar, NavItem, Nav, NavDropdown, MenuItem, Button, FormGroup, FormControl, Form } from 'react-bootstrap';
+
+const Header = (props) => {
+  return(
+    <div>
+      <Navbar inverse fixedTop>
+        
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="">THE VALAR</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        
+        <Navbar.Collapse>
+
+          <Navbar.Form pullRight>
+            <FormGroup>
+              <FormControl type="text" placeholder="username" />
+              <FormControl type="text" placeholder="password" />
+            </FormGroup>
+            <Button type="submit" >Log In</Button>
+          </Navbar.Form>
+          
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">
+              EXPLORE
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              TOP 
+            </NavItem>
+          </Nav>
+
+        </Navbar.Collapse>
+
+      </Navbar>
+    </div>
+  )
+}
+
+export default Header;
