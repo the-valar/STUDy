e3b12ee7180e345dc4fb8dca94983d247453f176
Dan Guan <daniel.guan.bca@gmail.com>
2018-07-17 18:37:41 -0400

    Implement database saving Yelp API call results
    - Built query for mysql connection

--- server/db/models/_model.js
+++ server/db/models/_model.js
@@ -1,5 +1,25 @@
 const db = require('../db.js');
 
+let saveSpots = function(studySpotList) {
+  for (let spot = 0; spot < studySpotList.businesses.length; spot++) {
+    var currSpot = studySpotList.businesses[spot];
+    console.log(currSpot);
+    db.query(
+      `INSERT INTO locations (id, name, city, state, address) VALUES 
+      ('${currSpot.id}', '${currSpot.name}', '${currSpot.location.city}', '${currSpot.location.state}', 
+      '${currSpot.location.address1}')`, 
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(result);
+        }
+      }
+    );
+    console.log('done');
+  }
+};
+
 module.exports = {
-    // Database interactions
-}
+  saveSpots: saveSpots
+};
