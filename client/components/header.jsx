dba440b266b0500c379470e53bada9c5e16909fb
Erik <elin7780@bths.edu>
2018-07-17 19:35:31 -0400

    Work on navbar, registration and login

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, NavItem, Nav, NavDropdown, MenuItem, Button, FormGroup, FormControl, Form } from 'react-bootstrap';
+import { Navbar, NavItem, Nav, NavDropdown, Well, MenuItem, Button, FormGroup, FormControl, Form, Modal } from 'react-bootstrap';
 import axios from 'axios';
 
 class Header extends React.Component {
@@ -7,12 +7,20 @@ class Header extends React.Component {
     super(props);
     this.state = {
       username: '',
-      password: ''
+      password: '',
+      showLogin: false,
+      showSignup: false
     }
 
     this.handleUser = this.handleUser.bind(this);
     this.handlePassword = this.handlePassword.bind(this);
     this.loginUser = this.loginUser.bind(this);
+
+    this.showLogin = this.showLogin.bind(this);
+    this.closeLogin = this.closeLogin.bind(this);
+
+    this.showSignup = this.showSignup.bind(this);
+    this.closeSignup = this.closeSignup.bind(this);
   }
 
   componentDidMount() {
@@ -42,6 +50,30 @@ class Header extends React.Component {
     })
   }
 
+  closeLogin() {
+    this.setState({
+      showLogin: false
+    })
+  }
+
+  showLogin() {
+    this.setState({
+      showLogin: true
+    })
+  }
+
+  closeSignup() {
+    this.setState({
+      showSignup: false
+    })
+  }
+
+  showSignup() {
+    this.setState({
+      showSignup: true
+    })
+  }
+
   render() {
     return (
       <div>
@@ -55,14 +87,35 @@ class Header extends React.Component {
           </Navbar.Header>
           
           <Navbar.Collapse>
+          
+          <Nav pullRight>
+            <NavItem onClick={this.showLogin}> Login </NavItem>
+            <NavItem onClick={this.showSignup}> Register </NavItem>
 
-          <Navbar.Form pullRight>
-            <FormGroup>
-              <FormControl type="text" placeholder="username" value={this.state.username} onChange={(e) => {this.handleUser(e)}} />
-              <FormControl type="password" placeholder="password" value={this.state.password} onChange={(e) => {this.handlePassword(e)}} />
-            </FormGroup>
-            <Button type="submit" value="Submit" onClick={this.handleClick}>Log In</Button>
-          </Navbar.Form>
+            <Modal show={this.state.showLogin} onHide={this.closeLogin}>
+              <Modal.Body>
+                <FormGroup>
+                  <FormControl type="text" placeholder="username" value={this.state.username} onChange={(e) => {this.handleUser(e)}} />
+                  <FormControl type="password" placeholder="password" value={this.state.password} onChange={(e) => {this.handlePassword(e)}} />
+                </FormGroup>
+                
+                <Button type="submit" value="Submit" onClick={this.handleClick}>Log In</Button>
+              </Modal.Body>
+            </Modal>
+
+            <Modal show={this.state.showSignup} onHide={this.closeSignup}>
+              <Modal.Body>
+                <FormGroup>
+                  <FormControl type="text" placeholder="username" value={this.state.username} onChange={(e) => {this.handleUser(e)}} />
+                  <FormControl type="password" placeholder="password" value={this.state.password} onChange={(e) => {this.handlePassword(e)}} />
+                </FormGroup>
+                
+                <Button type="submit" value="Submit" onClick={this.handleClick}> Register </Button>
+              </Modal.Body>
+            </Modal>
+
+
+          </Nav>
 
           </Navbar.Collapse>
 
