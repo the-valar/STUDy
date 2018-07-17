25a88596f0ebdb4aaff998e1352ffe1888e73fc9
Erik <elin7780@bths.edu>
2018-07-17 14:20:16 -0400

    Ready

--- client/index.jsx
+++ client/index.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/header.jsx';
 import Search from './components/Search.jsx';
-<<<<<<< HEAD
 
 import Header from './components/header.jsx';
 import Display from './components/Display.jsx';
-=======
->>>>>>> dev
 
 class App extends React.Component {
   constructor(props) {
@@ -19,13 +16,10 @@ class App extends React.Component {
       <div>
         <Header />
         <Search />
-<<<<<<< HEAD
         
         <div>
           <Display />
         </div>
-=======
->>>>>>> dev
       </div>
     )
   }
