00f762227932a14348557c4bbb079eff6fcf63ff
Dan Guan <daniel.guan.bca@gmail.com>
2018-07-18 19:57:01 -0400

    Complete search algo
    - Algorithm prioritizes locations with reviews that fit your preferences
    - If no reviews, return locations in order they appear in Yelp API response

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,20 +5,40 @@ const models = require('../db/models/_model.js');
 
 const app = express();
 
-app.use(express.static(__dirname + '/../../client'))
+app.use(express.static(__dirname + '/../../client'));
 app.use(parser.json());
 
 app.get('/search', (req, res) => {
   var params = req.query;
-  getClosestWithinRadius(params.location, 4000)
-  .then((studySpotList) => {
-    models.saveSpots(studySpotList.data);
-    res.send(studySpotList.data);
-  })
-  .catch((err) => {
-    console.log('Error searching for location: ' + err);
-  });
-})
+  var coffeeMult = 5 - params.coffee;
+  var atmosphereMult = 5 - params.atmosphere;
+  var comfortMult = 5 - params.comfort;
+  var foodMult = 5 - params.food;
+  getClosestWithinRadius(params.location, params.radius)
+    .then((studySpotList) => {
+      models.saveSpots(studySpotList.data.businesses);
+      if (coffeeMult + atmosphereMult + comfortMult + foodMult === 20) {
+        res.send(studySpotList.data);
+      } else {
+        models.getRelevantFirst(
+          studySpotList.data.businesses,
+          coffeeMult,
+          atmosphereMult,
+          comfortMult,
+          foodMult,
+          (err, results) => {
+            if (err) {
+              console.log(err);
+            }
+            res.send(results);
+          }
+        );
+      }
+    })
+    .catch((err) => {
+      console.log('Error searching for location: ' + err);
+    });
+});
 
 app.get('/login', (req, res) => {
   // both login and register req.query should have [username, password] as keys
@@ -32,7 +52,7 @@ app.get('/login', (req, res) => {
   });
 });
 
-app.post('/register', (req ,res) => {
+app.post('/register', (req, res) => {
   models.register(req.body, (err, data) => {
     if (err) {
       console.error('Username is taken');
@@ -42,7 +62,7 @@ app.post('/register', (req ,res) => {
   });
 });
 
-app.post('/addRating', (req, res) => {
+app.post('/ratings', (req, res) => {
   // req.body should have [user_id, location_id, coffeeTea, atmosphere, comfort, food] as keys
 
   models.addRating(req.body, (err, data) => {
@@ -50,7 +70,7 @@ app.post('/addRating', (req, res) => {
   });
 });
 
-app.get('/getRating', (req, res) => {
+app.get('/ratings', (req, res) => {
   // req.query should have location_id as a key
 
   models.getRating(req.query, (err, data) => {
@@ -62,7 +82,7 @@ app.get('/getRating', (req, res) => {
   });
 });
 
-app.post('/addFavorite', (req, res) => {
+app.post('/favorites', (req, res) => {
   // req.body should have user_id and location_id as keys
 
   models.addFavorite(req.body, (err, data) => {
@@ -70,7 +90,7 @@ app.post('/addFavorite', (req, res) => {
   });
 });
 
-app.get('/getFavorite', (req, res) => {
+app.get('/favorites', (req, res) => {
   // req.query should have user_id as a key
 
   models.getFavorite(req.query, (err, data) => {
@@ -82,7 +102,7 @@ app.get('/getFavorite', (req, res) => {
   });
 });
 
-app.post('/addComment', (req, res) => {
+app.post('/comments', (req, res) => {
   // req.body should have user_id, location_id, text as keys
 
   models.addComment(req.body, (err, data) => {
@@ -90,7 +110,7 @@ app.post('/addComment', (req, res) => {
   });
 });
 
-app.get('/getComment', (req, res) => {
+app.get('/comments', (req, res) => {
   // req.query should have location_id as a key
 
   models.getComment(req.query, (err, data) => {
@@ -102,7 +122,7 @@ app.get('/getComment', (req, res) => {
   });
 });
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log('App is listening to port', port);
