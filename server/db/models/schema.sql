a85dd0c1b0a3bd03624e755d74c478c291189e7b
Erik <elin7780@bths.edu>
2018-07-18 10:47:41 -0400

    Changed database structure, added new tables comments and user_ocations

--- server/db/models/schema.sql
+++ server/db/models/schema.sql
@@ -2,28 +2,42 @@ CREATE DATABASE IF NOT EXISTS greenfield;
 
 USE greenfield;
 
+CREATE TABLE IF NOT EXISTS users (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ratings (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(255),
+  coffeeTea INT,
+  atmosphere INT,
+  comfort INT,
+  food INT,
+  user_id INT,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
 CREATE TABLE IF NOT EXISTS locations (
   id VARCHAR(255) PRIMARY KEY,
   name VARCHAR(255),
   city VARCHAR(255),
   state VARCHAR(255),
-  address VARCHAR(255)
+  address VARCHAR(255),
+  rating_id INT,
+  FOREIGN KEY (rating_id) REFERENCES ratings(id)
 );
 
-CREATE TABLE IF NOT EXISTS ratings (
-  id INT PRIMARY KEY,
-  coffeeTea INT,
-  atmostphere INT,
-  comfort INT,
-  food INT,
-  location VARCHAR(255),
-  FOREIGN KEY (location) REFERENCES locations(id)
+CREATE TABLE IF NOT EXISTS users_locations (
+  user_id INT,
+  location_id VARCHAR(255)
 );
 
-CREATE TABLE  IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS comments (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(255) NOT NULL,
-  password VARCHAR(32) NOT NULL,
-  location INT,
-  FOREIGN KEY (location) REFERENCES location(id)
+  username VARCHAR(255),
+  text VARCHAR(255),
+  user_id INT,
+  FOREIGN KEY (user_id) REFERENCES users(id) 
 );
