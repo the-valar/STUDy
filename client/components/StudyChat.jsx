1219d86d13a6730f4f27f58e0c561eeb6a1e3c45
Martin <MartinGlyer@gmail.com>
2018-07-31 15:43:38 -0400

    writes skeleton of StudyChat component

---
+++ client/components/StudyChat.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class StudyChat extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            //some state
+        }
+        //bind methods
+    }
+
+
+    //define methods
+
+
+    //render
+    render() {
+        return (
+            <div>
+                We're on the study chat page!!!
+            </div>
+        )
+    }
+}
