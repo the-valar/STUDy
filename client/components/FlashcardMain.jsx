e2507f406f04a7b94889744ba3d84c89d9c46fcf
Martin <MartinGlyer@gmail.com>
2018-08-01 10:12:04 -0400

    abandoning videochat

---
+++ client/components/FlashcardMain.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class FlashcardMain extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            //some state
+        }
+        //bind section
+    }
+    
+    //method section
+
+
+    //render section
+}
