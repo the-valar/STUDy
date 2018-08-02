95a3348b49bb2f03b16f5b428f6d734d44eeb33a
parkermuir <parker.muir@gmail.com>
2018-08-02 14:46:10 -0400

    Database model working for favorites retrieval

--- server/db/models/_model.js
+++ server/db/models/_model.js
@@ -221,8 +221,8 @@ let addFavorite = function({ user_id, location_id }, cb) {
 
 let getFavorite = function(user_id, cb) {
   var command = `SELECT id, name, city, state, address, image1, image2, image3
-                 FROM users_locations
-                 JOIN locations ON locations.id=users_locations.location_id
+                 FROM locations
+                 JOIN users_locations ON locations.id=users_locations.location_id
                  WHERE users_locations.user_id=${user_id}`;
 
   db.getConnection((err, conn) => {
