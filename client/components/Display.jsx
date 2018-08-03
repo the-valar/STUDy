211e784dcb8fffb9f7343a8a4e5502a0fbe2c494
parkermuir <parker.muir@gmail.com>
2018-08-03 15:55:17 -0400

    Page no longer rerenders during modal launch

--- client/components/Display.jsx
+++ client/components/Display.jsx
@@ -234,7 +234,7 @@ class Display extends React.Component {
     if (!this.props.cafes.length) {
       return null;
       // page shows search results after successful search
-    } else if (this.props.cafes.length > 0 && !this.props.showIndivCafe) {
+    } else {
       return (
         <div>
           <Tabs id="search-map" onSelect={this.handleSelect} activeKey={this.state.showMap ? 2 : 1}>
@@ -242,11 +242,11 @@ class Display extends React.Component {
             <Tab eventKey={2} title="Map"></Tab>
           </Tabs>
           {this.state.showMap ? (
-            <div style={{marginTop: '1%'}}>
+            <div style={{ marginTop: '1%' }}>
               <Map cafes={this.props.cafes} cafeView={this.cafeView} />
             </div>
           ) : (
-              <div style={{marginTop: '1%'}}>
+              <div style={{ marginTop: '1%' }}>
                 <StackGrid columnWidth={300} monitorImagesLoaded={true}>
                   {this.props.cafes.map((cafe) => {
                     return (
@@ -281,227 +281,189 @@ class Display extends React.Component {
                 <div style={{ marginBottom: '5%' }} className="parallax" /></div>
             )}
 
-        </div>
-      );
-    } else if (this.props.cafes.length > 0 && this.props.showIndivCafe) {
-      return (
-        <div>
 
-          <Modal
-            show={this.props.showIndivCafe}
-            onHide={() => this.props.renderIndivCafe(false)}
-            dialogClassName="cafeView-modal"
-          >
-            <Modal.Header closeButton>
-              <Modal.Title>{this.state.currentCafe.name}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <div align="center" style={{ marginBottom: 50 }}>
-                {/* current cafe name & avg star ratings */}
-                <h3>{this.state.currentCafe.name}</h3>
-                <ShowReviews
-                  reviews={this.state.currentCafeReviews.data}
-                  cafe={this.state.currentCafe}
-                />
+          <div>
+        {this.props.showIndivCafe ? (
+              <Modal
+                show={this.props.showIndivCafe}
+                onHide={() => this.props.renderIndivCafe(false)}
+                dialogClassName="cafeView-modal"
+              >
+                <Modal.Header closeButton>
+                  <Modal.Title>{this.state.currentCafe.name}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <div align="center" style={{ marginBottom: 8 }}>
+                    {/* current cafe name & avg star ratings */}
+                    <h3>{this.state.currentCafe.name}</h3>
+                    <ShowReviews
+                      reviews={this.state.currentCafeReviews.data}
+                      cafe={this.state.currentCafe}
+                    />
 
-            {this.props.loggedIn ? <Button onClick={this.addToFave} style={{marginTop: '5px'}}>
-              Add to Favorites
-            </Button> : null }
-            <Grid>
-              <Row>
-                <Col xs={6} md={2}>
-                  Coffee/Tea:{' '}
-                  <br />
-                  <StarRatings
-                    numberOfStars={5}
-                    rating={this.state.currentCafeAvgRating.coffeeTea || 0}
-                    starDimension="20px"
-                    starSpacing="1px"
-                    starRatedColor="gold"
-                    starEmptyColor="grey"
-                  />
-                </Col>
-                <Col xs={6} md={2}>
-                  Atmosphere:{' '}
-                  <br />
-                  <StarRatings
-                    numberOfStars={5}
-                    rating={this.state.currentCafeAvgRating.atmosphere || 0}
-                    starDimension="20px"
-                    starSpacing="1px"
-                    starRatedColor="gold"
-                    starEmptyColor="grey"
-                  />
-                </Col>
-                <Col xs={6} md={2}>
-                  Comfort:{' '}
-                  <br />
-                  <StarRatings
-                    numberOfStars={5}
-                    rating={this.state.currentCafeAvgRating.comfort || 0}
-                    starDimension="20px"
-                    starSpacing="1px"
-                    starRatedColor="gold"
-                    starEmptyColor="grey"
-                  />
-                </Col>
-                <Col xs={6} md={2}>
-                  Food:{' '}
-                  <br />
-                  <StarRatings
-                    numberOfStars={5}
-                    rating={this.state.currentCafeAvgRating.food || 0}
-                    starDimension="20px"
-                    starSpacing="1px"
-                    starRatedColor="gold"
-                    starEmptyColor="grey"
-                  />
-                </Col>
-              </Row>
-            </Grid>
-            <AmazonBar creditCard = {this.props.creditCard}/>
+                    {this.props.loggedIn ? <Button onClick={this.addToFave} style={{ marginTop: '5px' }}>
+                      Add to Favorites
+            </Button> : null}
+                    <Grid>
+                      <Row>
+                        <Col xs={6} md={2}>
+                          Coffee/Tea:{' '}
+                          <br />
+                          <StarRatings
+                            numberOfStars={5}
+                            rating={this.state.currentCafeAvgRating.coffeeTea || 0}
+                            starDimension="20px"
+                            starSpacing="1px"
+                            starRatedColor="gold"
+                            starEmptyColor="grey"
+                          />
+                        </Col>
+                        <Col xs={6} md={2}>
+                          Atmosphere:{' '}
+                          <br />
+                          <StarRatings
+                            numberOfStars={5}
+                            rating={this.state.currentCafeAvgRating.atmosphere || 0}
+                            starDimension="20px"
+                            starSpacing="1px"
+                            starRatedColor="gold"
+                            starEmptyColor="grey"
+                          />
+                        </Col>
+                        <Col xs={6} md={2}>
+                          Comfort:{' '}
+                          <br />
+                          <StarRatings
+                            numberOfStars={5}
+                            rating={this.state.currentCafeAvgRating.comfort || 0}
+                            starDimension="20px"
+                            starSpacing="1px"
+                            starRatedColor="gold"
+                            starEmptyColor="grey"
+                          />
+                        </Col>
+                        <Col xs={6} md={2}>
+                          Food:{' '}
+                          <br />
+                          <StarRatings
+                            numberOfStars={5}
+                            rating={this.state.currentCafeAvgRating.food || 0}
+                            starDimension="20px"
+                            starSpacing="1px"
+                            starRatedColor="gold"
+                            starEmptyColor="grey"
+                          />
+                        </Col>
+                      </Row>
+                    </Grid>
+                    <AmazonBar creditCard={this.props.creditCard} />
 
-            <Carousel bsClass='sidebysideRight carousel'>
-              <Carousel.Item>
-                <img
-                  style={{ maxWidth: '600px', height: '50%' }}
-                  alt="600x200"
-                  src={this.state.currentCafe.pics[0]}
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  style={{ maxWidth: '600px', height: '50%' }}
-                  alt="600x200"
-                  src={this.state.currentCafe.pics[1]}
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  style={{ maxWidth: '600px', height: '50%' }}
-                  alt="600x200"
-                  src={this.state.currentCafe.pics[2]}
-                />
-              </Carousel.Item>
-            </Carousel>
-            <br />
-
-            {/* info of current cafe */}
-            <Grid>
-              <Row>
-                <Col
-                  xs={6}
-                  md={4}
-                  key={this.state.currentCafe.location.display_address}
-                >
-                  <p>
-                    <strong>Address:</strong>
+                    <Carousel bsClass='sidebysideRight carousel'>
+                      <Carousel.Item>
+                        <img
+                          style={{ maxWidth: '600px', height: '50%' }}
+                          alt="600x200"
+                          src={this.state.currentCafe.pics[0]}
+                        />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img
+                          style={{ maxWidth: '600px', height: '50%' }}
+                          alt="600x200"
+                          src={this.state.currentCafe.pics[1]}
+                        />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img
+                          style={{ maxWidth: '600px', height: '50%' }}
+                          alt="600x200"
+                          src={this.state.currentCafe.pics[2]}
+                        />
+                      </Carousel.Item>
+                    </Carousel>
                     <br />
-                    {this.state.currentCafe.location.address1}
-                    {this.state.currentCafe.location.address2 ? (
-                      <span>
-                        {' '}
-                        <br /> {this.state.currentCafe.location.address2}{' '}
-                      </span>
-                    ) : (
-                      ''
-                    )}
-                    {this.state.currentCafe.location.address3 ? (
-                      <span>
-                        {' '}
-                        <br /> {this.state.currentCafe.location.address3}{' '}
-                      </span>
-                    ) : (
-                      ''
-                    )}
-                    <br />
-                    {this.state.currentCafe.location.city},{' '}
-                    {this.state.currentCafe.location.state},{' '}
-                    {this.state.currentCafe.location.zip_code}
+
+                    {/* info of current cafe */}
+                    <Grid>
+                      <Row>
+                        <Col
+                          xs={6}
+                          md={4}
+                          key={this.state.currentCafe.location.display_address}
+                        >
+                          <p>
+                            <strong>Address:</strong>
+                            <br />
+                            {this.state.currentCafe.location.address1}
+                            {this.state.currentCafe.location.address2 ? (
+                              <span>
+                                {' '}
+                                <br /> {this.state.currentCafe.location.address2}{' '}
+                              </span>
+                            ) : (
+                                ''
+                              )}
+                            {this.state.currentCafe.location.address3 ? (
+                              <span>
+                                {' '}
+                                <br /> {this.state.currentCafe.location.address3}{' '}
+                              </span>
+                            ) : (
+                                ''
+                              )}
+                            <br />
+                            {this.state.currentCafe.location.city},{' '}
+                            {this.state.currentCafe.location.state},{' '}
+                            {this.state.currentCafe.location.zip_code}
+                          </p>
+                        </Col>
+                        <Col xs={6} md={4} key={this.state.currentCafe.phone}>
+                          <p>
+                            <strong>Phone:</strong>
+                            <br />
+                            {this.state.currentCafe.phone}
+                          </p>
+                        </Col>
+                        <Col xs={6} md={4} key={this.state.currentCafe.distance}>
+                          <p>
+                            <strong>Distance:</strong>
+                            <br />
+                            {(this.state.currentCafe.distance * 0.000621371).toFixed(
+                              2
+                            )}{' '}
+                            miles
                   </p>
-                </Col>
-                <Col xs={6} md={4} key={this.state.currentCafe.phone}>
-                  <p>
-                    <strong>Phone:</strong>
-                    <br />
-                    {this.state.currentCafe.phone}
-                  </p>
-                </Col>
-                <Col xs={6} md={4} key={this.state.currentCafe.distance}>
-                  <p>
-                    <strong>Distance:</strong>
-                    <br />
-                    {(this.state.currentCafe.distance * 0.000621371).toFixed(
-                      2
-                    )}{' '}
-                    miles
-                  </p>
-                </Col>
-              </Row>
-            </Grid>
+                        </Col>
+                      </Row>
+                    </Grid>
 
-            <div>
-              <Review
-                username={this.props.username}
-                userId={this.props.userId}
-                loggedIn={this.props.loggedIn}
-                review={this.state.review}
-                coffeeRating={this.state.coffeeRating}
-                atmosphereRating={this.state.atmosphereRating}
-                comfortRating={this.state.comfortRating}
-                foodRating={this.state.foodRating}
-                handleCoffee={this.handleCoffee}
-                handleAtmosphere={this.handleAtmosphere}
-                handleComfort={this.handleComfort}
-                handleFood={this.handleFood}
-                enterReview={this.enterReview}
-                submitReview={this.submitReview}
-              />
-            </div>
+                    <div>
+                      <Review
+                        username={this.props.username}
+                        userId={this.props.userId}
+                        loggedIn={this.props.loggedIn}
+                        review={this.state.review}
+                        coffeeRating={this.state.coffeeRating}
+                        atmosphereRating={this.state.atmosphereRating}
+                        comfortRating={this.state.comfortRating}
+                        foodRating={this.state.foodRating}
+                        handleCoffee={this.handleCoffee}
+                        handleAtmosphere={this.handleAtmosphere}
+                        handleComfort={this.handleComfort}
+                        handleFood={this.handleFood}
+                        enterReview={this.enterReview}
+                        submitReview={this.submitReview}
+                      />
+                    </div>
+                  </div>
+                </Modal.Body>
+              </Modal>
+            ) : null}
+
           </div>
-            </Modal.Body>
-          </Modal>
-
-
-          {/* current cafe name & picture */}
-          
-
-          {/* list of cafes from search */}
-          <Tabs id="search-map" onSelect={this.handleSelect} activeKey={this.state.showMap ? 2 : 1}>
-            <Tab eventKey={1} title="Results"></Tab>
-            <Tab eventKey={2} title="Map"></Tab>
-          </Tabs>
-          {this.state.showMap ? (
-            <div style={{marginTop: '1%'}}>
-              <Map cafes={this.props.cafes} cafeView={this.cafeView} />
-            </div>
-          ) : (
-          
-          <div style={{marginTop: '1%'}}>
-          <StackGrid columnWidth={300} monitorImagesLoaded={true}>
-            {this.props.cafes.map((cafe) => {
-              return (
-                <div key={cafe.id}>
-                  <Thumbnail
-                    src={cafe.image_url}
-                    height="250"
-                    onClick={() =>   this.cafeView(cafe)}
-                  >
-                    <h3>{cafe.name}</h3>
-                    <p>
-                      {cafe.location.address1}, {cafe.location.city},{' '}
-                      {cafe.location.state}, {cafe.location.zip_code}
-                    </p>
-                  </Thumbnail>
-                </div>
-              );
-            })}
-          </StackGrid>
-
-          <div className="parallax" />
-          </div>
-          )}
-          {/* Modal popup when review is submitted */}
+        )
+          }
+           
           <Modal
             id="reviewSubmissionModal"
             show={this.state.submittedReview}
