a8129f549da071dc972597550ffa3c12cb58acc7
Martin <MartinGlyer@gmail.com>
2018-08-03 13:58:40 -0400

    writes successful test

--- spec/spec.js
+++ spec/spec.js
@@ -1,5 +1,8 @@
 const assert = require('assert')
 const expect = require('chai').expect
+const React = require('react')
+const { mount } = require('enzyme')
+// const { spy } = require('sinon')
 
 describe('Array', function() {
     describe('#indexOf()', function() {
@@ -33,11 +36,17 @@ describe('Array', function() {
 
 
 /////Martin Test
+// const FlashcardMain = require('./../client/components/FlashcardMain.jsx')
+const db = require('./../server/db/models/_model')
 
-describe('New Deck Creation', () => {
-  describe('Save Card', () => {
-    it('should add the front and back as a new card to the array',() => {
-      
+// spy(FlashcardMain.prototype, 'componentDidMount')
+
+describe('database', () => {
+  describe('fetch Deck names', () => {
+    it('should receive an array of names from the db', () => {
+        db.fetchDeckNames(1, (err, docs) => { 
+          expect(docs[0]).to.have.property('title')
+        })
     })
   })
 })
